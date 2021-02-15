5090163f018e7dfb8e80cc1ce01d122d0ee68038
Fran <franipfp@gmail.com>
2021-02-16 00:21:17 +0100

    cleaned some comments and added stylelint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import '../styles/app.css';
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   element.innerHTML = 'Hello with PostCSS';
-//   element.classList.add('hello');
-
-//   return element;
-// }
-
-// document.body.appendChild(component());
